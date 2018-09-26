47c6e3a659843904d0a35bda2ffa498d10d74412
Christopher Medlin <christopherjmedlin@gmail.com>
2018-09-26 06:31:20 -0700

    Add success page for add vendor

--- veggienet/vendors/views.py
+++ veggienet/vendors/views.py
@@ -2,7 +2,7 @@ from .models import Vendor
 from .forms import AddVendorForm
 from veggienet.db import save_to_database
 from veggienet.util.geo import geocode_address
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, redirect
 
 vendors_views_bp = Blueprint("vendors_views", __name__)
 
@@ -20,4 +20,9 @@ def add_vendor():
         vendor = Vendor(form.name.data, form.address.data,
                         form.vendor_type.data, latlon)
         save_to_database(vendor)
+        return redirect(url_for("vendors_views.add_vendor_success"))
     return render_template("pages/vendors/add-vendor.html", form=form)
+
+@vendors_views_bp.route("/add-vendor/success")
+def add_vendor_success():
+    return render_template("pages/vendors/add-vendor-success.html")
